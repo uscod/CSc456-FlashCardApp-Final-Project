4bf934f4c891ef3a76d46eded6ca1c3e88a5d576
Myat Thu Ko (Tony) <nymyatthuko@gmail.com>
2023-04-12 23:11:27 -0400

    Initial commit of the Xcode project

---
+++ StudySwift/StudySwift/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StudySwift
+//
+//  Created by Myat Thu Ko on 4/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StudySwift/StudySwift/StudySwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySwiftApp.swift
+//  StudySwift
+//
+//  Created by Myat Thu Ko on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct StudySwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
